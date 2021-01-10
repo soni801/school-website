1a3ead7204f2c4e94966ea2e434f48e632e18f4e
soni801 <32430506+soni801@users.noreply.github.com>
2021-01-10 15:05:51 +0100

    Complete mobile menu

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function openMenu()
 {
-    document.getElementById("menu").style.width = "250px";
+    document.getElementById("menu").style.width = "80%";
+
     document.getElementById("close-button").style.visibility = "visible";
     document.getElementById("close-button").style.opacity = "1";
+
+    document.getElementById("overlay").style.visibility = "visible";
+    document.getElementById("overlay").style.opacity = ".5";
+
+    document.getElementById("links").style.visibility = "visible";
+    document.getElementById("links").style.opacity = "1";
 }
 
 function closeMenu()
 {
     document.getElementById("menu").style.width = "0";
+
     document.getElementById("close-button").style.visibility = "hidden";
     document.getElementById("close-button").style.opacity = "0";
+
+    document.getElementById("overlay").style.visibility = "hidden";
+    document.getElementById("overlay").style.opacity = "0";
+
+    document.getElementById("links").style.visibility = "hidden";
+    document.getElementById("links").style.opacity = "0";
 }
