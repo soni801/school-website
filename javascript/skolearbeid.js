49e867dda1b39ff6ba1adc0b0c75f44caaa645e8
soni801 <32430506+soni801@users.noreply.github.com>
2021-05-02 21:50:47 +0200

    Dynamically load content

---
+++ javascript/skolearbeid.js
@@ -0,0 +1,14 @@
+fetch("../content/skolearbeid.json")
+    .then(response => response.json())
+    .then(data =>
+    {
+        for (let i = 0; i < data.skolearbeid.length; i++)
+        {
+            document.getElementById("content").innerHTML +=
+                "<div id='" + data.skolearbeid[i].title.replace(/\s/g, "") + "'>" +
+                "<img src='" + data.skolearbeid[i].image + "' alt='" + data.skolearbeid[i].title + "'>" +
+                "<h2>" + data.skolearbeid[i].title + "</h2>" +
+                "<p>" + data.skolearbeid[i].content + "</p>" +
+                "</div>";
+        }
+    });
