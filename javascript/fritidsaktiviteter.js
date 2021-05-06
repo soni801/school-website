c7143d50541fc5c742748252c498e05ee48195fb
soni801 <32430506+soni801@users.noreply.github.com>
2021-05-06 19:32:40 +0200

    Load content to fritidsaktiviteter

---
+++ javascript/fritidsaktiviteter.js
@@ -0,0 +1,16 @@
+fetch("../content/fritidsaktiviteter.json")
+    .then(response => response.json())
+    .then(data =>
+    {
+        for (let i = 0; i < data.fritidsaktiviteter.length; i++)
+        {
+            document.getElementById("content").innerHTML +=
+                "<div id='" + data.fritidsaktiviteter[i].title.replace(/\s/g, "") + "'>" +
+                "<img src='" + data.fritidsaktiviteter[i].image + "' alt='" + data.fritidsaktiviteter[i].title + "'>" +
+                "<div>" +
+                "<h2>" + data.fritidsaktiviteter[i].title + "</h2>" +
+                "<p>" + data.fritidsaktiviteter[i].content + "</p>" +
+                "</div>" +
+                "</div>";
+        }
+    });
