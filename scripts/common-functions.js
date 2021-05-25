999a468947a9faa0b33d7c797b8469828237fdb2
soni801 <72191139+jegkjedermeg@users.noreply.github.com>
2021-05-25 15:31:24 +0200

    Store theme in localStorage

--- scripts/common-functions.js
+++ scripts/common-functions.js
@@ -1,7 +1,31 @@
+// Load sections into document
 $("#header-pre").load("/sections/header.html");
 $("#footer-pre").load("/sections/footer.html");
 
+// Load theme on page launch
+try
+{
+    loadTheme(localStorage.getItem("theme"));
+}
+catch (e)
+{
+    loadTheme("light");
+}
+
+// Toggle website theme
 function toggleTheme()
+{
+    const html = document.querySelector("html");
+    const style = getComputedStyle(html);
+
+    const light = style.getPropertyValue("--light");
+    const preserveLight = style.getPropertyValue("--preserve-light");
+
+    if (light === preserveLight) loadTheme("dark");
+    else loadTheme("light");
+}
+
+function loadTheme(theme)
 {
     const html = document.querySelector("html");
     const style = getComputedStyle(html);
@@ -12,14 +36,16 @@ function toggleTheme()
     const preserveDark = style.getPropertyValue("--preserve-dark");
     const preserveLight = style.getPropertyValue("--preserve-light");
 
-    if (light === preserveLight)
+    switch (theme)
     {
-        html.style.setProperty("--dark", light);
-        html.style.setProperty("--light", dark);
-    }
-    else
-    {
-        html.style.setProperty("--dark", preserveDark);
-        html.style.setProperty("--light", preserveLight);
+        case "dark":
+            html.style.setProperty("--dark", light);
+            html.style.setProperty("--light", dark);
+            localStorage.setItem("theme", "dark");
+            break;
+        case "light":
+            html.style.setProperty("--dark", preserveDark);
+            html.style.setProperty("--light", preserveLight);
+            localStorage.setItem("theme", "light");
     }
 }
