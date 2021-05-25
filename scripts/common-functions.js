f4dc9e6af22082504b8f2cfae9065e17c1a0a4bc
soni801 <72191139+jegkjedermeg@users.noreply.github.com>
2021-05-25 10:20:25 +0200

    Split content into smaller files

--- scripts/common-functions.js
+++ scripts/common-functions.js
@@ -1,23 +1,2 @@
-// Resize iframes on load
-/*window.onload = function ()
-{
-    const iframes = document.getElementsByTagName("iframe");
-
-    for (let i = 0; i < iframes.length; i++)
-    {
-        iframes[i].style.height = iframes[i].offsetWidth / 16 * 9 + "px";
-    }
-};*/
-
-// Resize iframes on window resize
-/*$(window).on('resize',function ()
-{
-    const iframes = document.getElementsByTagName("iframe");
-
-    for (let i = 0; i < iframes.length; i++)
-    {
-        iframes[i].style.height = iframes[i].offsetWidth / 16 * 9 + "px";
-    }
-});*/
-
-// TODO: Themes
+$("#header-pre").load("/sections/header.html");
+$("#footer-pre").load("/sections/footer.html");
