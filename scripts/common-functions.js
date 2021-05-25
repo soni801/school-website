a2879de998bdc41d6e5b56f5fc871d11c2dc099c
soni801 <72191139+jegkjedermeg@users.noreply.github.com>
2021-05-25 14:10:02 +0200

    Make theme toggle-able

--- scripts/common-functions.js
+++ scripts/common-functions.js
@@ -1,2 +1,25 @@
 $("#header-pre").load("/sections/header.html");
 $("#footer-pre").load("/sections/footer.html");
+
+function toggleTheme()
+{
+    const html = document.querySelector("html");
+    const style = getComputedStyle(html);
+
+    const dark = style.getPropertyValue("--dark-mode");
+    const light = style.getPropertyValue("--light");
+
+    const preserveDark = style.getPropertyValue("--preserve-dark");
+    const preserveLight = style.getPropertyValue("--preserve-light");
+
+    if (light === preserveLight)
+    {
+        html.style.setProperty("--dark", light);
+        html.style.setProperty("--light", dark);
+    }
+    else
+    {
+        html.style.setProperty("--dark", preserveDark);
+        html.style.setProperty("--light", preserveLight);
+    }
+}
